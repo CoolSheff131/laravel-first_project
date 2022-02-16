f87b07105b19e46bc48d37fcff2841918af00e44
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-16 23:11:03 +0800

    миграции

--- app/Services/Post/Service.php
+++ app/Services/Post/Service.php
@@ -3,23 +3,36 @@
 namespace App\Services\Post;
 
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 
 class Service
 {
     public function store($data)
     {
-        $tags = $data['tags'];
-        unset($data['tags']);
-        $post = Post::create($data);
-        $post->tags()->attach($tags);
-        return $post;
+        try {
+            DB::beginTransaction();
+            $tags = $data['tags'];
+            unset($data['tags']);
+            $post = Post::create($data);
+            $post->tags()->attach($tags);
+            DB::commit();
+            return $post;
+        } catch (\Exception $exception) {
+            DB::rollBack();
+        }
     }
 
     public function update($post, $data)
     {
-        $tags = $data['tags'];
-        unset($data['tags']);
-        $post->update($data);
-        $post->tags()->sync($tags);
+        try {
+            DB::beginTransaction();
+            $tags = $data['tags'];
+            unset($data['tags']);
+            $post->update($data);
+            $post->tags()->sync($tags);
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+        }
     }
 }
