8d14b4c4b88ec9712ba2e80fa3a54794393f7116
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-16 16:59:21 +0800

    показ валидации

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -5,22 +5,31 @@
         @csrf
         <div class="form-group">
             <label for="title">Title</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="title">
+            <input value="{{old('title')}}" type="text" name="title" class="form-control" id="title" placeholder="title">
+            @error('title')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="form-group">
             <label for="content">Content</label>
-            <textarea class="form-control" name="content" id="content" placeholder="Content"></textarea>
+            <textarea class="form-control" name="content" id="content" placeholder="Content">{{old('content')}}</textarea>
+            @error('content')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="form-group">
             <label for="image">Image</label>
-            <input type="text" class="form-control" name="image" id="image" placeholder="Image">
+            <input value="{{old('image')}}" type="text" class="form-control" name="image" id="image" placeholder="Image">
+            @error('image')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="category">Category</label>
             <select class="form-control" id="category" name="category_id">
                 @foreach($categories as $category)
-                <option value="{{ $category->id }}">{{ $category->title}}</option>
+                <option {{old('category_id') == $category->id ? 'selected': ''}} value="{{ $category->id }}">{{ $category->title}}</option>
                 @endforeach
             </select>
         </div>
