d66d6026659f766bbc83c74ad9cbd21b8953de3d
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-16 20:49:59 +0800

    АДминка

--- app/Http/Controllers/Admin/Post/IndexController.php
+++ app/Http/Controllers/Admin/Post/IndexController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Http\Controllers\Post;
+namespace App\Http\Controllers\Admin\Post;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\PostFilter;
 use App\Http\Requests\Post\FilterRequest;
 use App\Models\Post;
 
-class IndexController extends BaseController
+class IndexController extends Controller
 {
     public function __invoke(FilterRequest $request)
     {
@@ -15,6 +15,6 @@ class IndexController extends BaseController
         $filter = app()->make(PostFilter::class, ['queryParams' => array_filter($data)]);
         $posts = Post::filter($filter)->paginate(10);
 
-        return view('post.index', compact('posts'));
+        return view('admin.post.index', compact('posts'));
     }
 }
